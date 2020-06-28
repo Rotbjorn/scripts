e4a712812c6cab90135044267ec944734d895a10
Rotbjorn <rotbjorn@outlook.com>
2020-06-28 07:50:34 +0200

    Added some features

--- documentation/minecraft/spigot/get-spigot.md
+++ documentation/minecraft/spigot/get-spigot.md
@@ -12,22 +12,34 @@ curl -o get-spigot.kts https://raw.githubusercontent.com/Rotbjorn/scripts/master
 ## Usage
 Using [kscript](https://github.com/holgerbrandl/kscript):
 
-`kscript get-spigot.kts <version>`
+`kscript get-spigot.kts -v <version>`
 
-**or**
+_**or** with shebang:_
 
-`./get-spigot.kts <version>`
+`./get-spigot.kts -v <version>`
 
 \
 Alternatively using the standard Kotlin compiler:
 
-`kotlinc -script get-spigot.kts`
+`kotlinc -script get-spigot.kts -- -v <version>`\
+_Note: make sure to insert program arguments after `--`._
+
+## Example
+
+
+Using [kscript](https://github.com/holgerbrandl/kscript): \
+`kscript get-spigot.kts -v 1.16.1 -nodebug`\
+`./get-spigot.kts -nodebug -v 1.15.2`
+
+Using the standard Kotlin compiler: \
+`kotlinc -script get-spigot.kts -- -v 1.14`
 
 ## Parameters
 
 ##### Required
-- <version\> - All available **versions** can be found on the Spigot website [here](https://www.spigotmc.org/wiki/buildtools/#versions).
+* `-v <version>` - All available **versions** can be found on the Spigot website [here](https://www.spigotmc.org/wiki/buildtools/#versions).
 
-
+##### Optional
+* `-nodebug` - Disables building and cleanup output.
 
 
--- scripts/minecraft/spigot/get-spigot.kts
+++ scripts/minecraft/spigot/get-spigot.kts
@@ -7,82 +7,185 @@ import java.nio.file.Path
 import java.nio.file.Paths
 import java.util.*
 import kotlin.system.exitProcess
+import kotlin.system.measureTimeMillis
 
-if (args.isEmpty()) {
+
+
+if (args.size < 2) {
     System.err.println("Specify what version; ")
-    System.err.println("eg:    ./get-spigot.kts 1.16.1")
+    System.err.println("eg:    ./get-spigot.kts -- -v 1.16.1")
     System.err.println("You can find what versions are acceptable on: https://www.spigotmc.org/wiki/buildtools/#versions")
+    System.err.println("")
     exitProcess(0)
 }
 
-val inputStream: InputStream = URL("https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar").openStream()
+var debug = true
+var version = ""
 
-val currentDirectory: Path = Paths.get("").toAbsolutePath()
+//region Parameter
+data class Command(var command: String, var argument: String) {
+    class CommandBuilder(private val noParam: Boolean = false) {
 
-val buildToolsDirectory = File("$currentDirectory/BuildTools/")
+        var commandName: String? = null
+            private set
+        var commandArgument: String? = null
+            private set
+        var isActive = false
 
-val buildToolsFile = File("$currentDirectory/BuildTools/BuildTools.jar")
+        fun setCommand(_commandName: String): CommandBuilder {
+            isActive = true
+            commandName = _commandName
+            if (noParam) {
+                isActive = false
+                commandArgument = "true"
+            }
 
-if (!buildToolsFile.exists()) {
-    buildToolsFile.parentFile.mkdirs()
+            return this
+        }
+
+        fun setArgument(_arg: String): CommandBuilder {
+            isActive = true
+            commandArgument = _arg
+            return this
+        }
+
+        fun build(): Command {
+            isActive = false
+            return Command(commandName!!, commandArgument!!)
+        }
+
+    }
 }
 
-Files.copy(inputStream, buildToolsFile.toPath())
+var commands = mutableMapOf<String, String>()
+var cb = Command.CommandBuilder()
+for (arg in args) {
+    if (arg.startsWith('-')) {
+        if (arg.length < 2) {
+            System.err.println("Invalid command!")
+            exitProcess(0)
+        }
 
-execJar(buildToolsFile, args[0])
+        if (cb.isActive) {
+            System.err.println("No parameter specified after: ${cb.commandName}")
+            exitProcess(0)
+        }
 
-for (file in buildToolsDirectory.listFiles()!!) {
+        if (arg == "-nodebug") {
+            cb = Command.CommandBuilder(true)
+            cb.setCommand(arg.substring(1))
+            if (!cb.isActive) {
+                val command = cb.build()
+                commands[command.command] = command.argument
+            }
+        } else
+            cb = Command.CommandBuilder()
+        cb.setCommand(arg.substring(1))
+
+    } else if (cb.isActive) {
+        cb.setArgument(arg)
+        val command = cb.build()
+        commands[command.command] = command.argument
+    } else {
+        System.err.println("Illegal parameter usage!")
+        exitProcess(0)
+    }
+}
+
+if(cb.isActive) {
+    System.err.println("No parameter specified after: ${cb.commandName}")
+    exitProcess(0)
+}
+
+if(!commands.contains("v")) {
+    System.err.println("No version specified!")
+    exitProcess(0)
+}
+
+version = commands["v"].toString()
+if (commands["nodebug"].toString() == "true") {
+    debug = false
+}
+//endregion Parameter
+
+val startTime = System.currentTimeMillis()
+
+
+val currentDirectory: Path = Paths.get("").toAbsolutePath()
+val buildToolsFile = File("$currentDirectory/BuildTools/BuildTools.jar")
+
+
+// Creates ./BuildTools/ folder
+if (!buildToolsFile.parentFile.exists())
+    buildToolsFile.parentFile.mkdirs()
+
+
+// Gets the latest BuildTools.jar build and deploys it at ./BuildTools/BuildTools.jar
+Files.copy(URL("https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar").openStream(), buildToolsFile.toPath())
+
+
+// Automatically starts the BuildTools.jar
+val process: Process = ProcessBuilder(listOf<String>("java", "-jar", buildToolsFile.absolutePath, "--rev", version))
+        .directory(buildToolsFile.parentFile.absoluteFile)
+        .redirectErrorStream(true)
+        .start()
+
+
+
+val reader = BufferedReader(InputStreamReader(process.inputStream))
+
+var line: String? = ""
+do {
+    line = reader.readLine()
+    if(line == null)
+        break
+    printDebugLn(line!!)
+
+}while(true)
+
+println("""
+    
+    
+    
+    
+    BuildTools.jar DONE
+    
+    
+    
+    
+""".trimIndent())
+
+process.waitFor() // Wait for BuildTools.jar to finish
+
+// Moves the spigot.jar file to the script's directory
+for (file in buildToolsFile.parentFile.listFiles()!!) {
     if (file.name.startsWith("spigot")) {
         Files.move(file.toPath(), Paths.get("$currentDirectory/${file.name}"))
-        println("Moved ${file.name} to the script directory")
         break
     }
 }
 
-buildToolsDirectory.deleteRecursively()
 
+// Deletes the ./BuildTools/ directory
+buildToolsFile.parentFile.deleteRecursively()
 
-/*////////////////////////////////////////////////////////////////////////////
+val timeTaken = (System.currentTimeMillis().toDouble() - startTime.toDouble()) / 1000
 
-                                   Methods
-
-*/////////////////////////////////////////////////////////////////////////////
-
+println("get-script.kts execution time: ${timeTaken}s")
 
 fun File.deleteRecursively() {
-    val files = this.listFiles()
-    if(!files.isNullOrEmpty()) {
+    val files = listFiles()
+    if (!files.isNullOrEmpty()) {
         for (file in files) {
             file.deleteRecursively()
-            println("Deleted ${file.name} in ${file.absolutePath}")
+            printDebugLn("Deleted ${file.name} in ${file.absolutePath}")
         }
     }
-    this.delete()
+    delete()
 }
 
-
-
-
-fun execJar(jarFile: File, vararg arguments: String) {
-    val commandArguments = mutableListOf<String>(
-            "java",
-            "-jar",
-            jarFile.absolutePath,
-            "--rev"
-    )
-    commandArguments.addAll(arguments)
-
-    val builder = ProcessBuilder(commandArguments)
-    builder.directory(jarFile.parentFile.absoluteFile)
-    builder.redirectErrorStream(true)
-    val process = builder.start()
-
-    val sc = Scanner(process.inputStream)
-
-    while (sc.hasNextLine())
-        println(sc.nextLine())
-
-    sc.close()
-
-    process.waitFor()
+fun printDebugLn(str: Any) {
+    if(debug) {
+        println(str)
+    }
 }
